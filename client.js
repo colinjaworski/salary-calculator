3cfb42fc2d3b512f1b0529c9c9970a49e1790440
colin jaworski <colinrjaworski.84@gmail.com>
2022-01-29 14:09:52 -0600

    made function to push employee object to array of employees.

--- client.js
+++ client.js
@@ -6,3 +6,18 @@ function readyNow() {
     // on click, append box to DOM
 }    
 
+let employeeArray = [];
+
+
+function newEmployee(firstNameInput, lastNameInput, idInput, titleInput, annualSalaryInput){
+    let employeeObject = {
+        firstName: firstNameInput,
+        lastName: lastNameInput,
+        ID: idInput,
+        title: titleInput,
+        annualSalary: annualSalaryInput
+        }
+        employeeArray.push(employeeObject)
+  } 
+  console.log(newEmployee ('colin', 'jaworski', 47, 'manager', 12345));
+  
